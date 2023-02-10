08462c63b74c0349f5907641f2420a0d4e4f7cdb
Tergel Tsagaan <tergel.tsagaan@gmail.com>
2023-02-10 14:32:13 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BarComponent } from './bar/bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
