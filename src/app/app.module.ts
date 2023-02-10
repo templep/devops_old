571e3a9637db7be5778a5ff5f763e1c9bf6413df
Tergel Tsagaan <tergel.tsagaan@gmail.com>
2023-02-10 14:55:20 +0100

    a/b skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { AngularSplitModule } from "angular-split";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ProductPageComponent } from './product-page/product-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularSplitModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
