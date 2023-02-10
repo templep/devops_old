d338b0b784b6d1443d071158240b2f3a5d1c7d4d
Tergel Tsagaan <tergel.tsagaan@gmail.com>
2023-02-10 14:52:31 +0100

    example product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductPageComponent } from "./product-page/product-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: ProductPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/product-page/product.entity.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  name: string;
+  imageUrl: string;
+  price: number;
+  description: string;
+  category: string;
+  seller: string;
+}
